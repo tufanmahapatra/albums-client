5de710b00b30bb43918d752c2c56423a5a1437fa
tufanmahapatra <106529369+tufanmahapatra@users.noreply.github.com>
2022-06-01 18:12:09 +0530

    Add files via upload

---
+++ src/main/java/com/mudra/albums/AlbumsApplication.java
@@ -0,0 +1,19 @@
+package com.mudra.albums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the main entrypoint of the Server
+ * 
+ * @author viraj
+ *
+ */
+@SpringBootApplication
+public class AlbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumsApplication.class, args);
+	}
+
+}
